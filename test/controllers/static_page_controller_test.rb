667631686a54580f133c09d88705ba14fb4d7770
hoangnm132 <hoangnm132@gmail.com>
2019-06-13 09:54:28 +0700

    chapter 3 completed(should_get_root test need to be fixed )

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -5,6 +5,15 @@ def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+# will edit test "should get root" later
+
+  test "should get root" do
+    get 
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+
   test "should get home" do
     get static_page_home_url
     assert_response :success
