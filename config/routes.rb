5b719eee5893cf5934195fbbc1a374a066643142
hoangnm132 <hoangnm132@gmail.com>
2019-06-26 13:57:19 +0700

    chapter5 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'static_page#home'
-  get "static_page/home"
-  get "static_page/help"
-  get "static_page/about"
-  get "static_page/contact"
+  	get 'users/new'
+  	root 'static_page#home'
+  	get  "/help", to: "static_page#help"
+	get  "/about",  to: "static_page#about"
+	get "/contact", to: "static_page#contact"
+	get  '/signup',  to: 'users#new'
   
+ 
 end
