8710c877f484a27cc74bf094211ea66108264381
hoangnm132 <hoangnm132@gmail.com>
2019-06-27 09:20:11 +0700

    chapter 7 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  	get 'users/new'
+ 	get "users/new"
+  	get "users/new"
   	root 'static_page#home'
   	get  "/help", to: "static_page#help"
 	get  "/about",  to: "static_page#about"
 	get "/contact", to: "static_page#contact"
-	get  '/signup',  to: 'users#new'
+	get  "/signup",  to: 'users#new'
+	post "/signup",  to: 'users#create'
   
- 
+ 	resources :users
+
 end
