b2d64bc7c0d4b3f67de9245df502d923e1d2145f
hoangnm132 <hoangnm132@gmail.com>
2019-06-27 15:15:20 +0700

    chapter 7 fixing.04

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by id: params[:id]
     return if @user
-      flash[:error] = t (".notfound")
-      redirect_to root_path
+    flash[:error] = t (".notfound")
+    redirect_to root_path
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
-      flash[:success] = t (".welcome")
+      flash[:success] = t ".welcome"
       redirect_to @user
     else
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save { email.downcase }
+  before_save {email.downcase!}
   validates :name,  presence: true, length: { maximum: Settings.max_name_length }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: Settings.max_email_length }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
