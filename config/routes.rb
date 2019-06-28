b158772b425ffd377f32f781e888a9266db3307c
hoangnm132 <hoangnm132@gmail.com>
2019-06-28 14:30:40 +0700

    chapter8 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "sessions/new"
   root "static_page#home"
 
   get "users/new"
@@ -7,8 +8,13 @@ Rails.application.routes.draw do
   get "/about", to: "static_page#about"
   get "/contact", to: "static_page#contact"
   get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
 
+  post "/login", to: "sessions#create"
   post "/signup", to: "users#create"
+
+  delete "/logout", to: "sessions#destroy"
+
 
   resources :users
 end
