13c5d6a24ff56f73b68f5c7847e8b2d082ce3dc7
hoangnm132 <hoangnm132@gmail.com>
2019-06-28 16:30:41 +0700

    chapter8 fixing.02

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
+    unless !session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
