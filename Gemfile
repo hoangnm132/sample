3fe1083ee80b7c3c0cee5bf5851b7303cb9c52b5
hoangnm132 <hoangnm132@gmail.com>
2019-07-03 16:14:34 +0700

    chapter10 completed

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
 gem "bootsnap", ">= 1.1.0", require: false
 gem "jquery-rails", "~> 4.3", ">= 4.3.1"
+gem 'faker', '~> 1.9', '>= 1.9.4'
+gem 'will_paginate', '~> 3.1', '>= 3.1.7'
+gem 'bootstrap-will_paginate', '~> 1.0'
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def new
     @user = User.new
   end
@@ -21,8 +25,52 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find_by id: params[:id]
+  end
+
+  def update
+    @user = User.find_by id: params[:id]
+    if @user.update(user_params)
+      flash[:success] = t ".successed"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def admin_user
+    redirect_to root_url  unless current_user.admin?
+  end
+
+  def destroy
+    User.find_by(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "please_login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    unless @user == current_user
+      redirect_to root_url
+      flash[:danger] = t "not_permited"
+    end
   end
 end
