a5241169d4d702682baf3caf231efbe1894fccc6
hoangnm132 <hoangnm132@gmail.com>
2019-07-03 08:18:37 +0700

    chapter 9 fixing completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   attr_accessor :remember_token
   before_save {email.downcase!}
   validates :name, presence: true, length: { maximum: Settings.max_name_length }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: Settings.max_email_length }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
   has_secure_password
