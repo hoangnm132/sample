66d291713ed24a0dded300fbbf9313d60c7d6174
hoangnm132 <hoangnm132@gmail.com>
2019-07-03 08:04:04 +0700

    chapter 9 fixing.01

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,16 +26,15 @@ module SessionsHelper
     @current_user = nil
   end
 
-  def remember(user)
+  def remember user
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  def forget(user)
+  def forget user
     user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
-
 end
